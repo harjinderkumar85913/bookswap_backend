7ed48d71eed8ff76f4a291e33c08b5dba5359866
unknown <harjinder85913@gmail.com>
2026-03-04 18:37:59 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const express = require("express")
-const app = express()
-const port = 4009;
-const cors = require("cors")
-app.use(cors())
-require("dotenv").config()
-const config = require("./Config/db")
+const express = require("express");
+const app = express();
 
-const seeder = require("./Config/seeder")
-seeder.adminseeder()
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config();
+
+const config = require("./Config/db");
+
+const seeder = require("./Config/seeder");
+seeder.adminseeder();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-const route = require("./Routes/apiRoutes")
-app.use("/api",route)
 
-app.listen(port,()=>{
-    console.log("my project is running on port" + " " + port)
-})
+const route = require("./Routes/apiRoutes");
+app.use("/api", route);
+
+const port = process.env.PORT || 4009;
+
+app.listen(port, () => {
+    console.log("My project is running on port " + port);
+});
